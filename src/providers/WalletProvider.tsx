d2139c8e16641c3f0d350da4913c2aeb655b0723
Rui Santiago <ruisantiagomr@gmail.com>
2025-03-21 16:25:54 +0000

    add popup wallet (#13)
    Co-authored-by: Ruben Marcus <rubenmarcus.dev@gmail.com>

--- src/providers/WalletProvider.tsx
+++ src/providers/WalletProvider.tsx
@@ -2,19 +2,13 @@
 
 import { BitteWalletContextProvider } from "@bitte-ai/react";
 
-const BitteWalletSetup = {
-  network: "mainnet",
-  callbackUrl: typeof window !== "undefined" ? window.location.origin : "",
-  contractAddress: "",
-};
-
 type WalletProviderProps = {
   children: React.ReactNode;
 };
 
 const WalletProvider: React.FC<WalletProviderProps> = ({ children }) => {
   return (
-    <BitteWalletContextProvider {...BitteWalletSetup}>
+    <BitteWalletContextProvider>
       {children}
     </BitteWalletContextProvider>
   );
